5f6a7b5fa74171d0b71e30970d7f965c05df6e44
garcierl <garcierl@iu.edu>
2021-10-11 10:47:49 -0400

    Create app.js

---
+++ app/app.js
@@ -0,0 +1,19 @@
+function showContent(pageID){
+    console.log(pageID);
+}
+
+
+function initListeners(){
+    $(window).on("hashchange", function(e){
+        let navID = e.currentTarget.id;
+
+        MODEL.route(navID, showContent);
+    })
+}
+
+
+$(document).ready(function(){
+    initListeners();
+    MODEL.route("home", showContent);
+    // MODEL.route("home", showContent);
+})
