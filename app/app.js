3d28e435e0bf6c748ee37315ad1cabcaf066677b
garcierl <garcierl@iu.edu>
2021-10-11 11:12:43 -0400

    Update app.js

--- app/app.js
+++ app/app.js
@@ -2,11 +2,24 @@ function showContent(pageID){
     console.log(pageID);
 }
 
+function menu() {
+    console.log("working");
+    $(".menu-bars").on("click", () => {
+      $(".menu-bars").css("display", "none");
+      $(".menu-cross").css("display", "block");
+      $(".menu-dropdown").css("display", "flex");
+      $(".menu-cross").on("click", () => {
+        $(".menu-cross").css("display", "none");
+        $(".menu-bars").css("display", "block");
+        $(".menu-dropdown").css("display", "none");
+      });
+    });
+  }
 
 function initListeners(){
     $(window).on("hashchange", function(e){
         let navID = e.currentTarget.id;
-
+        menu();
         MODEL.route(navID, showContent);
     })
 }
@@ -14,6 +27,7 @@ function initListeners(){
 
 $(document).ready(function(){
     initListeners();
+    menu();
     MODEL.route("home", showContent);
     // MODEL.route("home", showContent);
 })
